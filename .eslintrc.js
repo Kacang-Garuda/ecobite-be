d57a70a06f2f5a76bdef3e9e9a4546a873ef5c8a
IrPanTech <irfannkamill@gmail.com>
2024-07-11 20:29:25 +0700

    setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,20 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error', // or "error"
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'prefer-const': 'off',
   },
-};
+}
