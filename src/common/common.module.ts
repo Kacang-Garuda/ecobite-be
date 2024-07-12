85a08d69e31427013a4495bc2bb3308cf5b0e910
IrPanTech <irfannkamill@gmail.com>
2024-07-13 03:58:59 +0700

    trying fix

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,13 +1,11 @@
 import { Global, Module } from '@nestjs/common'
 import { EncryptionUtil } from './utils/encryption.util'
 import { ResponseUtil } from './utils/response.util'
-import { Public } from './decorators/public.decorator'
-import { GetUser } from './decorators/getUser.decorator'
 
 @Global()
 @Module({
   providers: [ResponseUtil, EncryptionUtil],
-  exports: [ResponseUtil, EncryptionUtil, Public, GetUser],
+  exports: [ResponseUtil, EncryptionUtil],
   imports: [],
 })
 export class CommonModule {}
