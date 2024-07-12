6c3a9bc9de75d1fe21ba92329db68a8f5b2154d7
IrPanTech <irfannkamill@gmail.com>
2024-07-13 03:45:39 +0700

    deploy

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,11 +1,13 @@
 import { Global, Module } from '@nestjs/common'
 import { EncryptionUtil } from './utils/encryption.util'
 import { ResponseUtil } from './utils/response.util'
+import { Public } from './decorators/public.decorator'
+import { GetUser } from './decorators/getUser.decorator'
 
 @Global()
 @Module({
   providers: [ResponseUtil, EncryptionUtil],
-  exports: [ResponseUtil, EncryptionUtil],
+  exports: [ResponseUtil, EncryptionUtil, Public, GetUser],
   imports: [],
 })
 export class CommonModule {}
